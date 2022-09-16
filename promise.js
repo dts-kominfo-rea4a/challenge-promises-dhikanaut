3ad6111250a30da36cc3cf9c1b9096fb2b769b32
Dhika Nadia Utama <dhikanaut@gmail.com>
2022-09-16 15:47:59 +0700

    async/await

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const TheaterIXX = await promiseTheaterIXX();
+  const TheaterVGC = await promiseTheaterVGC();
+  let countemosi = 0;
+  
+  TheaterIXX.map((values) => {
+    if (values.hasil == emosi) countemosi += 1;
+  });
+  
+  TheaterVGC.map((values) => {
+    if (values.hasil == emosi) countemosi += 1;
+  });
+
+  return countemosi;
+};
 
 module.exports = {
   promiseOutput,
